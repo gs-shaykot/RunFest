9ab52fe14afdcda7f2c5b1936a196aed1bde4cf6
GS SHAYKOT <shaykotkhan1916@gmail.com>
2024-12-23 21:26:02 +0600

    marathons ID based GET API created

--- index.js
+++ index.js
@@ -42,6 +42,12 @@ async function run() {
             res.send(result)
         })
 
+        app.get('/marathons/all/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await MarathonCollection.findOne(query)
+            res.send(result)
+        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
