03ff15ca640e6fce476cc46015b7d588aa399f54
GS SHAYKOT <shaykotkhan1916@gmail.com>
2024-12-26 20:48:00 +0600

    limited marathon data fetch fixed

--- src/Layout/HomeLayout.jsx
+++ src/Layout/HomeLayout.jsx
@@ -14,6 +14,7 @@ const HomeLayout = () => {
             </Helmet>
             <Banner></Banner>
             <Benefit></Benefit>
+            <SixMarathon></SixMarathon>
         </div>
     );
 };
