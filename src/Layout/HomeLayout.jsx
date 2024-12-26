76dce25ece9d75c3654cf146f0f76111c2e7293d
GS SHAYKOT <shaykotkhan1916@gmail.com>
2024-12-26 20:45:51 +0600

    home layout added

---
+++ src/Layout/HomeLayout.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Banner from '../Components/Banner';
+import SixMarathon from '../Components/SixMarathon';
+import Upcoming from '../Components/Upcoming';
+import Benefit from '../Components/Benefit';
+import Review from '../Components/Review';
+import { Helmet } from 'react-helmet';
+
+const HomeLayout = () => {
+    return (
+        <div>
+            <Helmet>
+                <title>Home</title>
+            </Helmet>
+        </div>
+    );
+};
+
+export default HomeLayout;
