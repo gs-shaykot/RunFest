b2890341a3cd09373c4847bb566cf7462c8dbd51
GS SHAYKOT <shaykotkhan1916@gmail.com>
2024-12-26 20:33:40 +0600

    limit based marathon card added

---
+++ src/Components/SixMarathon.jsx
@@ -0,0 +1,39 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import MarathonC from './MarathonC';
+import { Bounce } from 'react-awesome-reveal';
+
+const SixMarathon = () => {
+    const [marathons, setMarathons] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:5111/marathons/home')
+            .then(response => {
+                setMarathons(response.data);
+            })
+            .catch(error => {
+                console.error("Error fetching marathons:", error);
+            });
+    }, []);
+
+    return (
+        <div className='container mx-auto'>
+            <Bounce>
+                <h1 className='mt-5 font-bebas text-center text-4xl md:text-5xl font-semibold underline mb-8'>
+                    Running Marathons
+                </h1>
+            </Bounce>
+
+            <div className='w-11/12 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+                {
+                    marathons.map((marathon) => (
+                        <MarathonC key={marathon._id} marathon={marathon}></MarathonC>
+                    ))
+                }
+            </div>
+        </div>
+    );
+};
+
+export default SixMarathon;
