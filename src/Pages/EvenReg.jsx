9e9ec68dab9784dccef08ae6d424f9062e0bccd2
GS SHAYKOT <shaykotkhan1916@gmail.com>
2024-12-26 20:52:31 +0600

    event registration and login added

---
+++ src/Pages/EvenReg.jsx
@@ -0,0 +1,160 @@
+import React, { useContext } from 'react';
+import Register from './Register';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../Provider/AuthProvider';
+import axios from 'axios';
+import Swal from 'sweetalert2';
+
+const EvenReg = () => {
+    const { user } = useContext(AuthContext)
+    const location = useLocation();
+    const navigate = useNavigate();
+    const { eventData } = location.state || {};
+
+    const handleRegistration = e => {
+        e.preventDefault()
+        // Upload the Applicant data to Apply API
+        const formData = new FormData(e.target);
+        const formVals = Object.fromEntries(formData.entries());
+        axios
+            .post('http://localhost:5111/applied', formVals)
+            .then((response) => {
+                Swal.fire({
+                    title: "Succeess",
+                    text: "Registration Successfull",
+                    icon: "success"
+                });
+            })
+            .catch((error) => {
+                console.error('Error posting data:', error.response ? error.response.data : error.message);
+            });
+        // Update the totalRegistrationCount+=1
+        axios.patch(`http://localhost:5111/marathons/all/${eventData._id}`, {
+            totalRegistrationCount: eventData.totalRegistrationCount + 1
+        })
+            .then(response => {
+                console.log("Update successful:", response.data);
+                navigate(`/marathons/all/${eventData._id}`)
+            })
+            .catch(error => {
+                Swal.fire({
+                    title: "Wrong Happened",
+                    text: "Error while Updating",
+                    icon: "error"
+                });
+                console.error("Error updating marathon:", error.message);
+            });
+    }
+
+    return (
+        <div>
+            <h1 className='mt-5 font-bebas text-center text-4xl md:text-5xl font-semibold underline mb-8'>
+                Running Marathons
+            </h1>
+            <form onSubmit={handleRegistration} className="w-full max-w-lg mx-auto p-4 bg-white shadow rounded">
+                <h2 className="text-2xl font-bold mb-4 text-center">Marathon Registration</h2>
+
+                {/* Email */}
+                <div className="mb-4">
+                    <label className="block font-medium mb-2">Email</label>
+                    <input
+                        type="email"
+                        name="email"
+                        value={user.email}
+                        readOnly
+                        className="w-full px-3 py-2 border rounded bg-gray-200"
+                    />
+                </div>
+
+                {/* Marathon Title */}
+                <div className="mb-4">
+                    <label className="block font-medium mb-2">Marathon Title</label>
+                    <input
+                        type="text"
+                        name="marathonTitle"
+                        value={eventData.marathonTitle}
+                        readOnly
+                        className="w-full px-3 py-2 border rounded bg-gray-200"
+                    />
+                </div>
+
+                <div className='grid grid-cols-2 gap-3'>
+                    {/* Start Date */}
+                    <div className="mb-4">
+                        <label className="block font-medium mb-2">Start Date</label>
+                        <input
+                            type="text"
+                            name="startDate"
+                            value={eventData.eventDate}
+                            readOnly
+                            className="w-full px-3 py-2 border rounded bg-gray-200"
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label className="block font-medium mb-2">Type</label>
+                        <input
+                            type="text"
+                            name="eventType"
+                            value={eventData.eventType}
+                            readOnly
+                            className="w-full px-3 py-2 border rounded bg-gray-200"
+                        />
+                    </div>
+                </div>
+
+                {/* First Name */}
+                <div className="mb-4">
+                    <label className="block font-medium mb-2">First Name</label>
+                    <input
+                        type="text"
+                        name="firstName"
+                        className="w-full px-3 py-2 border rounded"
+                        required
+                    />
+                </div>
+
+                {/* Last Name */}
+                <div className="mb-4">
+                    <label className="block font-medium mb-2">Last Name</label>
+                    <input
+                        type="text"
+                        name="lastName"
+                        className="w-full px-3 py-2 border rounded"
+                        required
+                    />
+                </div>
+
+                {/* Contact Number */}
+                <div className="mb-4">
+                    <label className="block font-medium mb-2">Contact Number</label>
+                    <input
+                        type="text"
+                        name="contactNumber"
+                        className="w-full px-3 py-2 border rounded"
+                        required
+                    />
+                </div>
+
+                {/* Additional Info */}
+                <div className="mb-4">
+                    <label className="block font-medium mb-2">Additional Info</label>
+                    <textarea
+                        name="additionalInfo"
+                        className="w-full px-3 py-2 border rounded"
+                        rows="4"
+                    ></textarea>
+                </div>
+
+                {/* Submit Button */}
+                <button
+                    type="submit"
+                    className="w-full bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-600"
+                >
+                    Register
+                </button>
+            </form>
+        </div>
+    );
+};
+
+export default EvenReg;
---
+++ src/Pages/LogIn.jsx
@@ -0,0 +1,141 @@
+import React, { useContext } from "react";
+import { NavLink, useLocation, useNavigate } from "react-router-dom";
+import Lottie from "lottie-react";
+import theGif from "./../assets/lottie/ani2.json";
+import { AuthContext } from "../Provider/AuthProvider";
+import Swal from 'sweetalert2'
+import axios from "axios";
+
+const LogIn = () => {
+
+    const { logInUser, logInGoogle } = useContext(AuthContext)
+    const navigate = useNavigate()
+    const location = useLocation()
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target)
+        const initialData = Object.fromEntries(formData.entries())
+        const { email, password } = initialData;
+
+        logInUser(email, password)
+            .then(res => {
+                Swal.fire({
+                    title: "Succeess",
+                    text: "User Created Successfully",
+                    icon: "success"
+                });
+                navigate(location?.state ? location.state : '/')
+                const user = { Name: res.user.displayName, email: email }
+                axios.post('http://localhost:5111/jwt', user, { withCredentials: true })
+                    .then(data => {
+                        console.log(data)
+                    })
+            })
+            .catch(error => {
+                Swal.fire({
+                    title: "Error",
+                    text: error.message,
+                    icon: "error"
+                });
+            })
+    };
+
+    const handleGoogleLogin = () => {
+        logInGoogle()
+            .then(res => {
+                Swal.fire({
+                    title: "Successfully☺️",
+                    text: "Successfully Loged in.",
+                    icon: "success"
+                });
+                navigate(location?.state ? location.state : '/')
+                const user = { Name: res.user.displayName, email: res.user.email }
+                axios.post('http://localhost:5111/jwt', user, { withCredentials: true })
+                    .then(data => {
+                        console.log(data)
+                    })
+            })
+            .catch(error => {
+                Swal.fire({
+                    title: error.message,
+                    icon: "error"
+                });
+            })
+    }
+
+    return (
+        <div>
+            <div className="hero min-h-screen">
+                <div className="hero-content grid grid-cols-1 lg:grid-cols-2 gap-12 justify-items-end">
+                    <div className="order-2 lg:order-first text-center lg:text-left">
+                        <Lottie
+                            animationData={theGif}
+                            loop
+                            className="w-full h-full"
+                        />
+                    </div>
+
+                    {/* Login Form */}
+                    <div className="card bg-base-100 w-11/12 shadow-2xl">
+                        <form className="card-body pb-0" onSubmit={handleLogin}>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input
+                                    type="email"
+                                    name="email"
+                                    placeholder="email"
+                                    className="input input-bordered"
+                                    required
+                                />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input
+                                    type="password"
+                                    name="password"
+                                    placeholder="password"
+                                    className="input input-bordered"
+                                    required
+                                />
+                                <label className="label">
+                                    <NavLink
+                                        to="/forgot-password"
+                                        className="label-text-alt link link-hover font-semibold"
+                                    >
+                                        Forgot password?
+                                    </NavLink>
+                                    <NavLink
+                                        to="/register"
+                                        className="label-text-alt link link-hover font-semibold"
+                                    >
+                                        Register
+                                    </NavLink>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary bg-primary hover:bg-primary text-secondary border-0">
+                                    Login
+                                </button>
+                            </div>
+                        </form>
+                        <div className="w-full flex justify-center py-4 px-8">
+                            <button
+                                className="w-full btn bg-transparent border-2 border-secondary hover:bg-secondary hover:border-0 hover:text-white"
+                                onClick={handleGoogleLogin}
+                            >
+                                Login with Google
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default LogIn;
