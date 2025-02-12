e1bf1cdb83f9a050609b8a3f32dbee6c52a4e514
GS SHAYKOT <shaykotkhan1916@gmail.com>
2025-02-12 19:25:16 +0600

    navbar fixed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,32 +1,49 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import { AuthContext } from '../Provider/AuthProvider';
-import Swal from 'sweetalert2'
+import Swal from 'sweetalert2';
 
 const Navbar = () => {
-    const { user, LogOut, setUser } = useContext(AuthContext)
+    const { user, LogOut, setUser } = useContext(AuthContext);
+    const [scrolling, setScrolling] = useState(false);
+
     const SignOut = () => {
         LogOut()
             .then(res => {
-                setUser()
+                setUser();
                 Swal.fire({
-                    title: "Succeess",
+                    title: "Success",
                     text: "Logged Out Successfully",
-                    icon: "success"
+                    icon: "success",
                 });
             })
             .catch(error => {
                 Swal.fire({
                     title: "ERROR",
                     text: error.message,
-                    icon: "error"
+                    icon: "error",
                 });
-            })
-    }
+            });
+    };
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 10) {
+                setScrolling(true);
+            } else {
+                setScrolling(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
 
     return (
-        <div>
-            <div className="container mx-auto navbar bg-base-100">
+        <div  className={`bg-base-100 fixed top-0 w-full z-50 ${scrolling ? 'shadow-lg' : ''}`}>
+            <div className={`container mx-auto navbar`}>
                 <div className="navbar-start">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -43,48 +60,23 @@ const Navbar = () => {
                                     d="M4 6h16M4 12h8m-8 6h16" />
                             </svg>
                         </div>
-                        {
-                            user ?
-                                <ul
-                                    tabIndex={0}
-                                    className="z-10 menu menu-sm dropdown-content bg-base-100 rounded-sm mt-3 w-52 p-2 shadow">
-                                    <li>
-                                        <NavLink to='/'>Home</NavLink>
-                                    </li>
-                                    <li>
-                                        <NavLink to='/allMarathon'>Marathons</NavLink>
-                                    </li>
-                                    <li>
-                                        <NavLink to='/result'>Leaderboard</NavLink>
-                                    </li>
-                                    <li>
-                                        <NavLink to='/contact'>Contact Us</NavLink>
-                                    </li>
-                                    <li>
-                                        <NavLink to='/dashboard'>Dashboard</NavLink>
-                                    </li>
-                                </ul> :
-                                <ul
-                                    tabIndex={0}
-                                    className="z-10 menu menu-sm dropdown-content bg-base-100 rounded-sm mt-3 w-52 p-2 shadow">
-                                    <li>
-                                        <NavLink to='/'>Home</NavLink>
-                                    </li>
-                                    <li>
-                                        <NavLink to='/allMarathon'>Marathons</NavLink>
-                                    </li>
-                                    <li>
-                                        <NavLink to='/contact'>Contact Us</NavLink>
-                                    </li>
-                                    <li>
-                                        <NavLink to='/login'>Login</NavLink>
-                                    </li>
-                                    <li>
-                                        <NavLink to='/register'>Register</NavLink>
-                                    </li>
-                                </ul>
-                        }
-
+                        {user ? (
+                            <ul tabIndex={0} className="z-10 menu menu-sm dropdown-content bg-base-100 rounded-sm mt-3 w-52 p-2 shadow">
+                                <li><NavLink to='/'>Home</NavLink></li>
+                                <li><NavLink to='/allMarathon'>Marathons</NavLink></li>
+                                <li><NavLink to='/result'>Leaderboard</NavLink></li>
+                                <li><NavLink to='/contact'>Contact Us</NavLink></li>
+                                <li><NavLink to='/dashboard'>Dashboard</NavLink></li>
+                            </ul>
+                        ) : (
+                            <ul tabIndex={0} className="z-10 menu menu-sm dropdown-content bg-base-100 rounded-sm mt-3 w-52 p-2 shadow">
+                                <li><NavLink to='/'>Home</NavLink></li>
+                                <li><NavLink to='/allMarathon'>Marathons</NavLink></li>
+                                <li><NavLink to='/contact'>Contact Us</NavLink></li>
+                                <li><NavLink to='/login'>Login</NavLink></li>
+                                <li><NavLink to='/register'>Register</NavLink></li>
+                            </ul>
+                        )}
                     </div>
                     <div className='flex justify-center items-center'>
                         <img className='w-14 h-14' src="https://i.ibb.co.com/bPjkfDc/logo.gif" alt="" />
@@ -93,46 +85,31 @@ const Navbar = () => {
                 </div>
                 <div className="navbar-end flex items-center gap-4">
                     <ul className="menu menu-horizontal hidden lg:flex px-1 gap-2">
-                        <li>
-                            <NavLink to='/'>Home</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to='/allMarathon'>Marathons</NavLink>
-                        </li>
-                        <li className={user?.email ? "block" : "hidden"}>
-                            <NavLink to='/result'>Leaderboard</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to='/contact'>Contact Us</NavLink>
-                        </li>
-                        <li className={user?.email ? "block" : "hidden"}>
-                            <NavLink to='/dashboard'>Dashboard</NavLink>
-                        </li>
+                        <li><NavLink to='/'>Home</NavLink></li>
+                        <li><NavLink to='/allMarathon'>Marathons</NavLink></li>
+                        <li className={user?.email ? "block" : "hidden"}><NavLink to='/result'>Leaderboard</NavLink></li>
+                        <li><NavLink to='/contact'>Contact Us</NavLink></li>
+                        <li className={user?.email ? "block" : "hidden"}><NavLink to='/dashboard'>Dashboard</NavLink></li>
                     </ul>
                     <div className="md:mr-2">
-                        {
-                            user ?
-                                <div className="dropdown dropdown-end">
-                                    <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-                                        <div className="w-8 md:w-10 rounded-full">
-                                            <img
-                                                referrerPolicy='no-referrer'
-                                                alt="Tailwind CSS Navbar component"
-                                                src={user?.photoURL} />
-                                        </div>
+                        {user ? (
+                            <div className="dropdown dropdown-end">
+                                <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                                    <div className="w-8 md:w-10 rounded-full">
+                                        <img referrerPolicy='no-referrer' alt="Avatar" src={user?.photoURL} />
                                     </div>
-                                    <ul
-                                        tabIndex={0}
-                                        className="menu bg-base-100 text-black menu-sm dropdown-content rounded-box z-30 mt-3 w-52 p-2 shadow">
-                                        <li><a>{user?.displayName}</a></li>
-                                        <li><a onClick={SignOut}>Logout</a></li>
-                                    </ul>
-                                </div> :
-                                <div className='gap-2 hidden md:flex'>
-                                    <NavLink to='/login' className='btn bg-primary text-white font-semibold'>LogIn</NavLink>
-                                    <NavLink to='/register' className={`btn border-2 border-secondary bg-transparent font-semibold`}>Register</NavLink>
                                 </div>
-                        }
+                                <ul tabIndex={0} className="menu bg-base-100 text-black menu-sm dropdown-content rounded-box z-30 mt-3 w-52 p-2 shadow">
+                                    <li><a>{user?.displayName}</a></li>
+                                    <li><a onClick={SignOut}>Logout</a></li>
+                                </ul>
+                            </div>
+                        ) : (
+                            <div className='gap-2 hidden md:flex'>
+                                <NavLink to='/login' className='btn bg-primary text-white font-semibold'>LogIn</NavLink>
+                                <NavLink to='/register' className='btn border-2 border-secondary bg-transparent font-semibold'>Register</NavLink>
+                            </div>
+                        )}
                     </div>
                 </div>
             </div>
--- src/Components/Result.jsx
+++ src/Components/Result.jsx
@@ -1,24 +1,26 @@
+// check why data is now showing. i consoled the data, the data is not coming from server. but checked non queried(localhost:5111/result) api which is working
 import { useContext, useEffect, useState } from "react";
-import axios from "axios"; // Ensure axios is imported
+import axios from "axios";
 import { AuthContext } from "../Provider/AuthProvider";
 
 const Result = () => {
     const [search, setSearch] = useState("");
     const [year, setYear] = useState("2024");
-    const [resData, setResData] = useState([]); // Stores fetched results
-    const { user } = useContext(AuthContext);
-
+    const [resData, setResData] = useState([]);
+    const { user, setLoader } = useContext(AuthContext);
     useEffect(() => {
-        if (!user?.email) return; // Ensure email exists before fetching
+        if (!user?.email) return;
 
-        axios.get(`https://assignment-11-server-green-kappa.vercel.app/result?email=${user.email}&year=${year}&search=${search}`, { withCredentials: true })
+        axios.get(`http://localhost:5111/result?email=${encodeURIComponent(user.email)}&year=${encodeURIComponent(year)}&search=${encodeURIComponent(search)}`)
             .then(response => {
-                setResData(response.data); // Store the fetched results
+                setResData(response.data);
+                console.log(response.data)
             })
             .catch(error => {
                 console.error("Error fetching results:", error.message);
             });
-    }, [user?.email, search, year]);
+
+    }, [search, year, user?.email]); // Triggers re-fetch on state change
 
     return (
         <div className="bg-gray-50 min-h-screen font-sans">
@@ -26,7 +28,7 @@ const Result = () => {
             {/* Hero Section */}
             <div className="relative bg-gray-900 h-64 mt-16 flex items-center justify-center">
                 <img
-                    src="https://i.ibb.co.com/SX91nv1Q/Marathan-news.webp"
+                    src="https://i.ibb.co/SX91nv1Q/Marathan-news.webp"
                     className="absolute w-full h-full object-cover opacity-50"
                     alt="Marathon Banner"
                 />
@@ -37,28 +39,26 @@ const Result = () => {
             </div>
 
             {/* Filters */}
-            <div className="mx-auto p-6 mt-8 bg-white shadow rounded-lg">
+            <div className="mx-auto p-6 mt-8 bg-white shadow rounded-lg container">
                 <div className="w-full flex gap-5">
                     <input
                         type="text"
                         placeholder="Search Runner or Bib #"
-                        className="input input-bordered w-full"
+                        className="input input-bordered w-10/12"
                         value={search}
                         onChange={(e) => setSearch(e.target.value)}
                     />
-                    <div>
-                        <select
-                            className="select select-bordered w-full max-w-xs"
-                            value={year}
-                            onChange={(e) => setYear(e.target.value)}
-                        >
-                            <option disabled>Year</option>
-                            <option>2025</option>
-                            <option>2024</option>
-                            <option>2023</option>
-                            <option>2022</option>
-                        </select>
-                    </div>
+                    <select
+                        className="select select-bordered w-full max-w-xs"
+                        value={year}
+                        onChange={(e) => setYear(e.target.value)}
+                    >
+                        <option disabled>Year</option>
+                        <option>2025</option>
+                        <option>2024</option>
+                        <option>2023</option>
+                        <option>2022</option>
+                    </select>
                 </div>
             </div>
 
@@ -79,10 +79,10 @@ const Result = () => {
                         <tbody>
                             {resData.length > 0 ? (
                                 resData.map((runner, index) => (
-                                    <tr key={runner.id}>
+                                    <tr key={runner._id}>
                                         <td>{index + 1}</td>
                                         <td>{runner.name}</td>
-                                        <td>{runner.bib}</td>
+                                        <td>{runner.RID}</td>
                                         <td>{runner.time}</td>
                                         <td>{runner.marathonTitle}</td>
                                     </tr>
@@ -100,5 +100,6 @@ const Result = () => {
             </div>
         </div>
     );
-}; 
+};
+
 export default Result;
